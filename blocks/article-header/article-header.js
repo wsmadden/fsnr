518fb44d5334256405f3024c0660a34e7b59348b
scott <scottmadden@adobe.com>
2023-06-20 21:52:38 -0400

    PM Clean up

--- blocks/article-header/article-header.js
+++ blocks/article-header/article-header.js
@@ -1,4 +1,4 @@
-import {getMetadata} from '../../scripts/scripts.js';
+import { getMetadata } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
 
@@ -11,13 +11,9 @@ export default async function decorate(block) {
     const background = document.querySelector("main .article-header-container");
     background.classList.add('cmp-section-background-hero-7');
 
+    // Decorates article body
     const article = document.querySelector("main .default-content-wrapper");
-
     article.innerHTML = articleTemplate(article);
- 
-    console.log(article);
-
-    console.log(articleTemplate(article));
 
 }
 
@@ -41,20 +37,3 @@ function articleTemplate(el) {
     </article>
     `
 }
-
-/*
-<article itemscope="" itemtype="http://schema.org/BlogPosting">
-  <header>
-    <h1 itemprop="headline">Firestone Liberia and Children’s Surgery International Put Smiles on the Faces of Liberia’s Children</h1>
-    <ul>
-      <li>
-        <time itemprop="dateCreated pubdate datePublished"> Fri January 20, 2023 </time>
-      </li>
-    </ul>
-  </header> 
-  <div class="article__pre-body"></div>
-  <div class="article__body" itemprop="articleBody"></div>     
-</article>
-*/
-
-
--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -3,13 +3,13 @@ import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 export default function decorate(block) {
 
   [...block.children].forEach((div) => {
-  
+
     console.log(div);
 
     div.className = "card";
 
     [...div.children].forEach((div) => {
-      console.log("length",div.children.length);
+      console.log("length", div.children.length);
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'card-image';
       else div.className = 'card-body';
     });
@@ -17,30 +17,4 @@ export default function decorate(block) {
 
   });
 
-//console.log(block);
-
-/*
-   const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    li.innerHTML = row.innerHTML;
-
-     const wrapper = document.createElement('div');
-    wrapper.classList.add('card');
-
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'card-image';
-      else div.className = 'card-body';
-    });
-
-    wrapper.innerHTML = li.innerHTML;
-    li.innerHTML = wrapper.outerHTML;
-
-    ul.append(li);
-
-  });
-  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-  block.textContent = '';
-  block.append(ul);
-  */
 }
