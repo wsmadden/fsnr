d09376a56f517f46f992727a18443fcc01ec26d2
scott <scottmadden@adobe.com>
2023-06-20 21:53:51 -0400

    PM Clean up

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -4,12 +4,9 @@ export default function decorate(block) {
 
   [...block.children].forEach((div) => {
 
-    console.log(div);
-
     div.className = "card";
 
     [...div.children].forEach((div) => {
-      console.log("length", div.children.length);
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'card-image';
       else div.className = 'card-body';
     });
